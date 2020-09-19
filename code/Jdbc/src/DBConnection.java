94ad04959577da87ab173787f0a9d665ae65a40b
SuperSumit <scnaikwadi@yahoo.com>
2020-09-19 13:22:12 +0530

    DB COnnection
    DB COnnection

---
+++ code/Jdbc/src/DBConnection.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class DBConnection {
+	public static void main(String[] args) {
+		try {
+			//==============Oracle================
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			Connection oracleCon=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "sumit", "root");
+			
+			System.out.println("Oracle Connection Successful....");
+			
+			//==============Mysql================
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			Connection mySqlcon=DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc", "root", "root");
+			
+			System.out.println("Mysql Connection Successful....");	
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}

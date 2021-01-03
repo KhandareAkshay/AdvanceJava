7698e97cdb7c470f08563348cc810bcf553ea8b6
SuperSumit <scnaikwadi@yahoo.com>
2021-01-03 12:40:17 +0530

    Hibernate Intro
    Hibernate Intro

---
+++ code/Hibernate_Intro/src/Main.java
@@ -0,0 +1,32 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.classic.Session;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Student stud = new Student();
+		stud.setId(1);
+		stud.setName("Abc");
+		stud.setEmail("Abc@gmail.com");
+		stud.setContact("9988776655");
+		
+		Configuration cfg = new AnnotationConfiguration();
+		cfg.configure("hibernate-cfg.xml");
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session session = sf.openSession();
+		Transaction trx = session.beginTransaction();
+		
+		
+		session.save(stud);
+		
+		trx.commit();
+		session.close();
+		sf.close();
+		
+	}
+
+}
